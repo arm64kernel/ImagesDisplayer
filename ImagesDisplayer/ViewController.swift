1e97b0f8f9878206ce02822d80cac1157dace7d1
Vadim Ohapkin <vadim.ohapkin@intexsoft.by>
2024-09-09 14:45:48 +0300

    Add base for UIKit

---
+++ ImagesDisplayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImagesDisplayer
+//
+//  Created by Vadim Ohapkin on 9.09.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
